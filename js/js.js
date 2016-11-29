0c6330555fea0552586bf660b965e8c5a6df5bbd
wenhuil <wenhuiliu.whl@gmail.com>
2016-11-29 03:24:44 -0800

    website init

---
+++ js/js.js
@@ -0,0 +1,83 @@
+// Freelancer Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 800, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            //top: 0
+        }
+    })
+
+
+    // Floating label headings for the contact form
+    $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+    /***************** Waypoints ******************/
+
+    $('.wp2').waypoint(function() {
+        $('.wp2').addClass('animated fadeInDown');
+    }, {
+        offset: '75%'
+    });
+
+    $(document).ready(function(){
+    
+        // hide .navbar first
+        $("#mainNav").hide();
+        var target_offset = $("#start-to-show-nav").offset().top;
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+                // set distance user needs to scroll before we fadeIn navbar
+                if ($(this).scrollTop() > target_offset) {
+                    $('#mainNav').fadeIn(100);
+                } else {
+                    $('#mainNav').fadeOut(300);
+                }
+            });
+        });
+    });
+
+    $('#portfoliolist').mixItUp({
+        animation: {
+            effects: 'fade', /* fade scale */
+            duration: 700 /* 600 */
+        },
+        selectors: {
+            target: '.portfolio-item', /* .mix */
+            filter: '.filter' /* .filter */
+        }
+    });
+        
+})(jQuery); // End of use strict
+
