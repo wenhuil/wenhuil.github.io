5b3546c9319dec8e78aa59b6c18ae41b377da388
wenhuil <wenhuiliu.whl@gmail.com>
2017-01-25 22:39:08 -0800

    add watercolor and photo

--- js/carousel.js
+++ js/carousel.js
@@ -7,20 +7,20 @@
 	};
 
 	var cache = {
-		$carouselContainer: $('.thumbnails-carousel').parent(),
-		$thumbnailsLi: $('.thumbnails-carousel li'),
-		$controls: $('.thumbnails-carousel').parent().find('.carousel-control')
+		$carouselContainer: $('#illus-work .thumbnails-carousel').parent(),
+		$thumbnailsLi: $('#illus-work .thumbnails-carousel li'),
+		$controls: $('#illus-work .thumbnails-carousel').parent().find('#illus-work .carousel-control')
 	};
 
 	function init() {
-		cache.$carouselContainer.find('ol.carousel-indicators').addClass('indicators-fix');
+		cache.$carouselContainer.find('#illus-work ol.carousel-indicators').addClass('indicators-fix');
 		cache.$thumbnailsLi.first().addClass('active-thumbnail');
 
 		if(!conf.backgroundControl) {
-			cache.$carouselContainer.find('.carousel-control').addClass('controls-background-reset');
+			cache.$carouselContainer.find('#illus-work .carousel-control').addClass('controls-background-reset');
 		}
 		else {
-			cache.$controls.height(cache.$carouselContainer.find('.carousel-inner').height());
+			cache.$controls.height(cache.$carouselContainer.find('#illus-work .carousel-inner').height());
 		}
 
 		if(conf.center) {
@@ -54,4 +54,120 @@
 
 })(window, jQuery);
 
-$('.thumbnails-carousel').thumbnailsCarousel();
+$('#water-work .thumbnails-carousel').thumbnailsCarousel();
+
+// for watercolor paintings
+;(function(window, $, undefined) {
+
+	var conf = {
+		center: true,
+		backgroundControl: false
+	};
+
+	var cache = {
+		$carouselContainer: $('#water-work .thumbnails-carousel').parent(),
+		$thumbnailsLi: $('#water-work .thumbnails-carousel li'),
+		$controls: $('#water-work .thumbnails-carousel').parent().find('#water-work .carousel-control')
+	};
+
+	function init() {
+		cache.$carouselContainer.find('#water-work ol.carousel-indicators').addClass('indicators-fix');
+		cache.$thumbnailsLi.first().addClass('active-thumbnail');
+
+		if(!conf.backgroundControl) {
+			cache.$carouselContainer.find('#water-work .carousel-control').addClass('controls-background-reset');
+		}
+		else {
+			cache.$controls.height(cache.$carouselContainer.find('#water-work .carousel-inner').height());
+		}
+
+		if(conf.center) {
+			cache.$thumbnailsLi.wrapAll("<div class='center clearfix'></div>");
+		}
+	}
+
+	function refreshOpacities(domEl) {
+		cache.$thumbnailsLi.removeClass('active-thumbnail');
+		cache.$thumbnailsLi.eq($(domEl).index()).addClass('active-thumbnail');
+	}	
+
+	function bindUiActions() {
+		cache.$carouselContainer.on('slide.bs.carousel', function(e) {
+  			refreshOpacities(e.relatedTarget);
+		});
+
+		cache.$thumbnailsLi.click(function(){
+			cache.$carouselContainer.carousel($(this).index());
+		});
+	}
+
+	$.fn.thumbnailsCarousel = function(options) {
+		conf = $.extend(conf, options);
+
+		init();
+		bindUiActions();
+
+		return this;
+	}
+
+})(window, jQuery);
+
+$('#water-work .thumbnails-carousel').thumbnailsCarousel();
+
+// for photos
+;(function(window, $, undefined) {
+
+	var conf = {
+		center: true,
+		backgroundControl: false
+	};
+
+	var cache = {
+		$carouselContainer: $('#photo-work .thumbnails-carousel').parent(),
+		$thumbnailsLi: $('#photo-work .thumbnails-carousel li'),
+		$controls: $('#photo-work .thumbnails-carousel').parent().find('#photo-work .carousel-control')
+	};
+
+	function init() {
+		cache.$carouselContainer.find('#photo-work ol.carousel-indicators').addClass('indicators-fix');
+		cache.$thumbnailsLi.first().addClass('active-thumbnail');
+
+		if(!conf.backgroundControl) {
+			cache.$carouselContainer.find('#photo-work .carousel-control').addClass('controls-background-reset');
+		}
+		else {
+			cache.$controls.height(cache.$carouselContainer.find('#photo-work .carousel-inner').height());
+		}
+
+		if(conf.center) {
+			cache.$thumbnailsLi.wrapAll("<div class='center clearfix'></div>");
+		}
+	}
+
+	function refreshOpacities(domEl) {
+		cache.$thumbnailsLi.removeClass('active-thumbnail');
+		cache.$thumbnailsLi.eq($(domEl).index()).addClass('active-thumbnail');
+	}	
+
+	function bindUiActions() {
+		cache.$carouselContainer.on('slide.bs.carousel', function(e) {
+  			refreshOpacities(e.relatedTarget);
+		});
+
+		cache.$thumbnailsLi.click(function(){
+			cache.$carouselContainer.carousel($(this).index());
+		});
+	}
+
+	$.fn.thumbnailsCarousel = function(options) {
+		conf = $.extend(conf, options);
+
+		init();
+		bindUiActions();
+
+		return this;
+	}
+
+})(window, jQuery);
+
+$('#photo-work .thumbnails-carousel').thumbnailsCarousel();
