51f743c5f5f6d35dbfea70c0517c33c024794e9e
wenhuil <wenhuiliu.whl@gmail.com>
2017-03-02 23:41:16 -0800

    add hancraft work

--- js/carousel.js
+++ js/carousel.js
@@ -171,3 +171,61 @@ $('#water-work .thumbnails-carousel').thumbnailsCarousel();
 })(window, jQuery);
 
 $('#photo-work .thumbnails-carousel').thumbnailsCarousel();
+
+// for handcraft
+;(function(window, $, undefined) {
+
+	var conf = {
+		center: true,
+		backgroundControl: false
+	};
+
+	var cache = {
+		$carouselContainer: $('#handcraft-work .thumbnails-carousel').parent(),
+		$thumbnailsLi: $('#handcraft-work .thumbnails-carousel li'),
+		$controls: $('#handcraft-work .thumbnails-carousel').parent().find('#handcraft-work .carousel-control')
+	};
+
+	function init() {
+		cache.$carouselContainer.find('#handcraft-work ol.carousel-indicators').addClass('indicators-fix');
+		cache.$thumbnailsLi.first().addClass('active-thumbnail');
+
+		if(!conf.backgroundControl) {
+			cache.$carouselContainer.find('#handcraft-work .carousel-control').addClass('controls-background-reset');
+		}
+		else {
+			cache.$controls.height(cache.$carouselContainer.find('#handcraft-work .carousel-inner').height());
+		}
+
+		if(conf.center) {
+			cache.$thumbnailsLi.wrapAll("<div class='center clearfix'></div>");
+		}
+	}
+
+	function refreshOpacities(domEl) {
+		cache.$thumbnailsLi.removeClass('active-thumbnail');
+		cache.$thumbnailsLi.eq($(domEl).index()).addClass('active-thumbnail');
+	}	
+
+	function bindUiActions() {
+		cache.$carouselContainer.on('slide.bs.carousel', function(e) {
+  			refreshOpacities(e.relatedTarget);
+		});
+
+		cache.$thumbnailsLi.click(function(){
+			cache.$carouselContainer.carousel($(this).index());
+		});
+	}
+
+	$.fn.thumbnailsCarousel = function(options) {
+		conf = $.extend(conf, options);
+
+		init();
+		bindUiActions();
+
+		return this;
+	}
+
+})(window, jQuery);
+
+$('#handcraft-work .thumbnails-carousel').thumbnailsCarousel();
